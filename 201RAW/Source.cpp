df75cc3cdd743293c1a58b582c63085fb9bcc446
Mikhail Sherkunkov <sherkunkov@gmail.com>
2023-07-24 01:41:53 +0800

    201 raw done

--- 201RAW/Source.cpp
+++ 201RAW/Source.cpp
@@ -4,89 +4,102 @@
 
 using namespace std;
 
-void swap(const std::string& filename_in, const std::string& filename_out)
-{
-	ifstream fin(filename_in);
 
-	if (fin.is_open())
-	{
-		ofstream fout;
-		fout.open(filename_out);
-
-		while (!fin.eof())
-		{
-			string MAC, IP;
-
-			fin >> IP >> MAC;
-
-			fout.width(30);
-			fout << std::left;
-			fout << MAC << IP << endl;
-
-		}
-		fout.close();
-
-		fin.close();
-	}
-	else
-	{
-		cerr << "Error! File not found!" << endl;
-	}
-	std::string command = "start notepad ";
-	command += filename_out;
-	system(command.c_str());
-}
-
-//host 201 - 1
-//{
-//	hardware ethernet	00:19 : 99 : b4 : c2 : ae;
-//	fixed - address		192.168.100.21;
-//}
-void dhcpd(const std::string& filename_in, const std::string& filename_out)
-{
-	ifstream fin(filename_in);
-
-	if(fin.is_open())
-	{
-		ofstream fout;
-		fout.open(filename_out);
-		int count = 1;
-		while(!fin.eof())
-		{
-			string ip, mac;
-			fin >> ip >> mac;
-			if(ip != "")
-			{
-				int width = 30;
-				fout << "host 201 - " + to_string(count) << "\n{\n";
-				fout.width(width);
-				fout << std::left;
-				fout << "\thardware ethernet" << mac << ";\n";
-				fout.width(width);
-				fout << std::left;
-				fout << "\tfixed - address" << ip << ";\n}\n" << endl;
-
-				count++;
-			}
-			
-		}
-		fout.close();
-		fin.close();
-	}
-	else
-	{
-		cerr << "Error! File not found!" << endl;
-	}
-	std::string command = "start notepad ";
-	command += filename_out;
-	system(command.c_str());
-}
+void swap(string*& array, int& size, const std::string& filename);
+string* file_read(const std::string& filename, int& size);
+void dhcpd(const std::string& filename, const string* array, const int& size);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
-	swap("201 RAW.txt", "201 ready.txt");
-	dhcpd("201 RAW.txt", "201 dhcpd.txt");
+	int size = 0;
 
+	string* input = file_read("201 RAW.txt", size);
+
+	swap(input, size, "201 ready.txt");
+	dhcpd("201 dhcpd.txt", input, size);
+
+}
+
+string* file_read(const std::string& filename, int& size)
+{
+	ifstream fin(filename);
+
+	string* array = nullptr;
+
+	if (fin.is_open())
+	{
+		size = 0;
+		while (!fin.eof())
+		{
+			string ip, mac;
+			fin >> ip >> mac;
+			size++;
+		}
+		size = --size * 2;
+
+		fin.clear();
+		fin.seekg(0, fin.beg);
+
+		array = new string[size]{};
+
+		for (int i = 0; i < size; i++)
+		{
+			fin >> array[i];
+		}
+
+		fin.close();
+	}
+	else
+	{
+		cerr << "Error! File not found!" << endl;
+	}
+	return array;
+}
+
+void swap(string*& array, int& size, const std::string& filename)
+{
+	ofstream fout;
+	fout.open(filename);
+
+	for (int i = 0; i < size; i += 2)
+	{
+		fout.width(20);
+		fout << std::left;
+		fout << array[i + 1];
+		fout << array[i] << endl;
+	}
+
+	fout.close();
+
+	std::string command = "start notepad ";
+	command += filename;
+	system(command.c_str());
+}
+
+void dhcpd(const std::string& filename, const string* array, const int& size)
+{
+	ofstream fout;
+	fout.open(filename);
+	int count = 1;
+
+	int width = 30;
+
+	for (int i = 0; i < size; i += 2, count++)
+	{
+		fout << "host 201 - " + to_string(count) << "\n{" << endl;
+		fout.width(width);
+		fout << std::left;
+		fout << "\thardware ethernet" << array[i + 1] << ";" << endl;
+		fout.width(width);
+		fout << std::left;
+		fout << "\tfixed - address" << array[i] << ";\n}\n" << endl;
+	}
+
+	fout.close();
+
+	std::string command = "start notepad ";
+	command += filename;
+	system(command.c_str());
 }
